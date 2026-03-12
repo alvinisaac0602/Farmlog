451b4dcfb4688ab74d169bc3cd357322de872c62
alvinisaac0602 <kiizaisaacalvin256@gmail.com>
2026-03-12 14:46:11 +0300

    added home and welcome screens

--- app/index.tsx
+++ app/index.tsx
@@ -14,6 +14,22 @@ export default function Index() {
       <Link href="/onboarding" style={{ marginTop: 20, fontSize: 18, color: "blue" }}>
         Go to Onboarding
       </Link>
+      <Link href="/sign-in" style={{ marginTop: 20, fontSize: 18, color: "blue" }}>
+        Go to Sign In
+      </Link>
+      <Link href="/sign-up" style={{ marginTop: 20, fontSize: 18, color: "blue" }}>
+        Go to Sign Up
+      </Link> 
+      <Link href="/settings" style={{ marginTop: 20, fontSize: 18, color: "blue" }}>
+        Go to Settings
+      </Link>
+      <Link href="/home" style={{ marginTop: 20, fontSize: 18, color: "blue" }}>
+        Go to Home
+      </Link>
+      <Link href="/welcome" style={{ marginTop: 20, fontSize: 18, color: "blue" }}>
+        Go to Welcome
+      </Link>
+        
     </View>
   );
 }
